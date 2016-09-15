caeab036171d4ec43aa3aa96e4143c76fe9a3abe
Alejandro Berenguer <alexberemart@gmail.com>
2016-09-15 18:01:35 +0200

    development

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -1,12 +1,29 @@
-angular.module('myApp', [ 'ui.router', 'app.Controllers', 'app.Factory', 'app.Directives', 'ngToast', 'ui.bootstrap', "ui.bootstrap.tpls"])
+angular
+    .module('starterApp', ['ui.router', 'ngMaterial', 'ngMessages', 'users', 'countries', 'medals'])
+    .config(['$mdThemingProvider', '$mdIconProvider', '$stateProvider', '$urlRouterProvider', function ($mdThemingProvider, $mdIconProvider, $stateProvider, $urlRouterProvider) {
 
-    .config(['$stateProvider', function ($stateProvider) {
+        $mdIconProvider
+            .defaultIconSet("./assets/svg/avatars.svg", 128)
+            .icon("menu", "./assets/svg/menu.svg", 24)
+            .icon("share", "./assets/svg/share.svg", 24)
+            .icon("google_plus", "./assets/svg/google_plus.svg", 512)
+            .icon("hangouts", "./assets/svg/hangouts.svg", 512)
+            .icon("twitter", "./assets/svg/twitter.svg", 512)
+            .icon("phone", "./assets/svg/phone.svg", 512);
+
+        $mdThemingProvider.theme('default')
+            .primaryPalette('blue')
+            .accentPalette('red');
 
         $stateProvider
             .state('dashboard', {
                 url: '/dashboard',
                 abstract: true,   //marca este estado como no navegable.
                 views: {
+                    head: {
+                        templateUrl: './src/main/webapp/app/src/head-menu/head-menu.html',
+                        controller: 'head-menu'
+                    },
                     content: {
                         template: '<div ui-view="dashboardContent"></div>'
                     }
@@ -16,13 +33,21 @@ angular.module('myApp', [ 'ui.router', 'app.Controllers', 'app.Factory', 'app.Di
                 url: '/main',
                 views: {
                     dashboardContent: {
-                        templateUrl: 'app/main/views/main.html',
-                        controller: 'main'
+                        templateUrl: './src/main/webapp/app/src/main/head-menu.html',
+                        controller: 'head-menu'
                     }
                 }
             })
-    }])
+            .state('dashboard.result', {
+                url: '/result',
+                views: {
+                    dashboardContent: {
+                        templateUrl: 'results/results.html',
+                        controller: 'results'
+                    }
+                }
+            })
 
-    .config(['$urlRouterProvider', '$httpProvider', function ($urlRouterProvider) {
         $urlRouterProvider.otherwise('/dashboard/main');
+
     }]);
---
+++ src/main/webapp/app/src/main/head-menu.js
@@ -0,0 +1,5 @@
+angular.module('users')
+    .controller('head-menu',
+        function ($scope, $http, $window, $state) {
+
+        });
