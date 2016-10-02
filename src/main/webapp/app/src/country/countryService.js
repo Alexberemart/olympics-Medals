4b962c503e8cacc41b9c51104997be2aefe03bb9
Alexberemart <alexberemart@gmail.com>
2016-10-02 08:49:38 +0200

    fix bug

--- src/main/webapp/app/src/country/countryService.js
+++ src/main/webapp/app/src/country/countryService.js
@@ -10,62 +10,62 @@
             {
                 id : 1,
                 name: 'Jamaica',
-                flagUrl: 'app/images/JAMA0001.gif'
+                flagUrl: 'app/images/JAMA0001.GIF'
             },
             {
                 id : 2,
                 name: 'United States',
-                flagUrl: 'app/images/UNST0001.gif'
+                flagUrl: 'app/images/UNST0001.GIF'
             },
             {
                 id : 3,
                 name: 'Canada',
-                flagUrl: 'app/images/CANA0001.gif'
+                flagUrl: 'app/images/CANA0001.GIF'
             },
             {
                 id : 4,
                 name: 'France',
-                flagUrl: 'app/images/FRAN0001.gif'
+                flagUrl: 'app/images/FRAN0001.GIF'
             },
             {
                 id : 5,
                 name: 'Russian Federation',
-                flagUrl: 'app/images/RUSS0001.gif'
+                flagUrl: 'app/images/RUSS0001.GIF'
             },
             {
                 id : 6,
                 name: 'Kazakhstan',
-                flagUrl: 'app/images/KAZK0001.gif'
+                flagUrl: 'app/images/KAZK0001.GIF'
             },
             {
                 id : 7,
                 name: 'Japan',
-                flagUrl: 'app/images/JAPA0001.gif'
+                flagUrl: 'app/images/JAPA0001.GIF'
             },
             {
                 id : 8,
                 name: 'Uzbekistan',
-                flagUrl: 'app/images/UZBK0001.gif'
+                flagUrl: 'app/images/UZBK0001.GIF'
             },
             {
                 id : 9,
                 name: 'Denmark',
-                flagUrl: 'app/images/DENM0001.gif'
+                flagUrl: 'app/images/DENM0001.GIF'
             },
             {
                 id : 10,
                 name: 'Belarus',
-                flagUrl: 'app/images/BLRU0001.gif'
+                flagUrl: 'app/images/BLRU0001.GIF'
             },
             {
                 id : 11,
                 name: 'Sweden',
-                flagUrl: 'app/images/SWDN0001.gif'
+                flagUrl: 'app/images/SWDN0001.GIF'
             },
             {
                 id : 12,
                 name: 'South Korea',
-                flagUrl: 'app/images/SKOR0001.gif'
+                flagUrl: 'app/images/SKOR0001.GIF'
             }
         ];
 
